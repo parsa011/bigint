fec7d0ad7073b1b9a5d4dc18d7057f269df02f80
parsa011 <parsa32569@gmail.com>
2021-11-13 12:58:43 +0330

    write get_big_int function and add raw mode add also a personal getch function and add raw mode impelement backspace in raw mode

--- bigint.c
+++ bigint.c
@@ -2,32 +2,48 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <ctype.h>
+#include <termios.h>
+#include <string.h>
 
 #include "definitions.h"
 
 int main() 
 {
-	big_int first;
+	big_int first, second;
 	get_big_int(&first);
-	printl(first.val);
+	get_big_int(&second);
+	//printl(first.val);
+	//printl(second.val);
 	return 0;
 }
+
 
 // functions declarations
 
 void get_big_int(big_int *buf)
 {
 	buf->val = malloc(BUFSIZE);
-	int c,len,bufsize = BUFSIZE;
+	int c,len = 0;
+	int bufsize = BUFSIZE;
 	while (true) {
-		c = getchar();
+		c = getch(false);
+		// if taht was backspace key , move cursor to left , and remove last char of buf->val by decreasing
+		// 	val->len
+		if (c == KEY_BS) {
+			if (len != 0)
+				len--;
+			delete_left_char();
+			continue;
+		}
 		if (c == EOL || c == EOF) {
+			// if that was '\n' or EOF , close the string
 			buf->val[len] = '\0';
 			break;
 		}else {
 			// check if c is digit , or its the first char that user inputed and its '-' (for negative numbers)
 			bool is_negative = len == 0 && c == '-';
 			if (isdigit(c) || is_negative) {
+				// assign sing of the buf when first char was '-'
 				if (is_negative)
 					buf->sign = -1;
 				// check if we need more space for our buf->val
@@ -36,8 +52,47 @@ void get_big_int(big_int *buf)
 					buf->val = realloc(buf->val,bufsize);
 				}
 				buf->val[len++] = c;
+				printf("%c",c);
 			}
 		}
 	}
 	buf->len = len;
+	// insert new line
+	printl("");
+}
+
+// terminal stuff
+
+static int getch(bool write)
+{
+	enter_raw_mode();
+	int c = getchar();
+	exit_raw_mode();
+	if (write)
+		printf("%c",c);
+	return c;
+}
+
+void enter_raw_mode()
+{
+	tcgetattr(0, &oterm);
+	memcpy(&term, &oterm, sizeof(term));
+	term.c_lflag &= ~(ICANON | ECHO);
+	term.c_cc[VMIN] = 1;
+	term.c_cc[VTIME] = 0;
+	tcsetattr(0, TCSANOW, &term);
+	atexit(exit_raw_mode);
+}
+
+void exit_raw_mode()
+{
+	tcsetattr(0, TCSANOW, &oterm);
+}
+
+void delete_left_char()
+{
+	// tricky move for clear last char of screen , move left , write SPACE , and again move left :)
+	CURSOR_LEFT(1);
+	printf(" ");
+	CURSOR_LEFT(1);
 }
--- definitions.h
+++ definitions.h
@@ -2,15 +2,31 @@
 #define EOL '\n'
 #define BUFSIZE 10
 
+#define CURSOR_LEFT(x) printf("\033[%dD", (x))
+#define CURSOR_RIGHT(x) printf("\033[%dC", (x))
+#define KEY_BS 0x7F // backslash key code
+
 typedef struct {
 	int len;		// len of val string
 	char *val;		// our big int will store here
 	int sign;		// sign of number , just can be -1 and 1
 } big_int;
 
+// ----------------------
+//
 // functions declarations
+//
+// ----------------------
 
 /*
  * 	get next big int from user and return it in big_int struct style
  */
 void get_big_int(big_int *);
+
+
+// terminal stuff
+static struct termios term, oterm;
+void enter_raw_mode();
+void exit_raw_mode();
+static int getch(bool); // bool write into screen or no
+void delete_left_char();
